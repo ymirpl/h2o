a82b90b843b326994905a4e7028623d59849173f
Arno Candel <arno.candel@gmail.com>
2014-01-17 17:29:32 -0800

    Add diagnostics to output layer

--- src/main/java/hex/NeuralNet.java
+++ src/main/java/hex/NeuralNet.java
@@ -643,7 +643,7 @@ public class NeuralNet extends ValidatedJob {
         rms_bias = new double[ls.length];
         mean_weight = new double[ls.length];
         rms_weight = new double[ls.length];
-        for( int y = 1; y < layers.length-1; y++ ) {
+        for( int y = 1; y < layers.length; y++ ) {
           final Layer l = layers[y];
           final int len = l._a.length;
 
@@ -847,11 +847,11 @@ public class NeuralNet extends ValidatedJob {
           sb.append("<th>").append("Weight (Mean, RMS)").append("</th>");
           sb.append("<th>").append("Bias (Mean, RMS)").append("</th>");
           sb.append("</tr>");
-          for (int i=1; i<model.layers.length-1; ++i) {
+          for (int i=1; i<model.layers.length; ++i) {
             sb.append("<tr>");
             sb.append("<td>").append("<b>").append(i).append("</b>").append("</td>");
             sb.append("<td>").append("<b>").append(model.layers[i].units).append("</b>").append("</td>");
-            sb.append("<td>").append(model.layers[i].getClass().getSimpleName()).append("</td>");
+            sb.append("<td>").append(model.layers[i].getClass().getSimpleName().replace("Vec","").replace("Chunk", "")).append("</td>");
             sb.append("<td>").append(model.layers[i].rate(train.training_samples)).append("</td>");
             sb.append("<td>").append(model.layers[i].l1).append("</td>");
             sb.append("<td>").append(model.layers[i].l2).append("</td>");
@@ -1124,8 +1124,7 @@ public class NeuralNet extends ValidatedJob {
     public static AtomicLong seed; // = new AtomicLong(new Random().nextLong());
 
     public static Random getRNG() {
-      //return water.util.Utils.getDeterRNG(seed.getAndIncrement());
-      return water.util.Utils.getDeterRNG(seed.get());
+      return water.util.Utils.getDeterRNG(seed.getAndIncrement());
     }
   }
 }
